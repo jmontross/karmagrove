81cb57658daa7fad049d48200545fc0d16119966
Joshua Montross <joshua.montross@gmail.com>
2013-02-07 12:51:12 -0800

    ads new images and about us change and link to nutrition facts

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,22 @@
+class AboutController < ApplicationController
+
+def index
+  #  @products = Product.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+   #   format.json { render json: @products }
+    end
+  end
+
+  def joy_coin
+  #  @products = Product.all
+
+    respond_to do |format|
+      format.html # joy_coin.html.erb
+   #   format.json { render json: @products }
+    end
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,8 @@ Karmagrove::Application.routes.draw do
 
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
 
-
+  match 'about' => 'about#index'
+  match 'joy_coin_facts' => 'about#joy_coin'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
