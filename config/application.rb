b5f22e18422d0fb20f91113a5550856bded22cc8
Joshua Montross <joshua.montross@gmail.com>
2013-02-26 10:47:07 -0800

    modfies view with email option for telling bout how to donate and ads config file for stripe

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,9 @@ module Karmagrove
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
+    #  for heroku to precompile for us...
+    config.assets.initialize_on_precompile = false
+
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
