0d8224d6c44aa105e2abff4fb61e2a6c7e56e36f
Joshua Montross <joshua.montross@gmail.com>
2013-03-03 17:00:11 -0800

    removes stupid whitespace

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -45,7 +45,6 @@ end
 
   # Get the credit card details submitted by the form
   token = params[:stripeToken]
-
   # Create the charge on Stripe's servers - this will charge the user's card
   begin
     charge = Stripe::Charge.create(
