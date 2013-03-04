925e851ec803080bb2c96e4faef1fcbee375210c
Joshua Montross <joshua.montross@gmail.com>
2013-03-03 17:45:26 -0800

    new js helper to get my js per page like he say - http://stackoverflow.com/questions/6571753/rails-3-1-asset-pipeline-how-to-load-controller-specific-scripts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
   end
+
+  def javascript(*files)
+    content_for(:head) { javascript_include_tag(*files) }
+  end
 end
