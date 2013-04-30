566ad7854ebc7b402b67612397eb763bc5775a53
Joshua Montross <joshua.montross@gmail.com>
2013-04-30 16:48:15 -0700

    ads karma grove tree to front page, makes about us into who are we... We enable conscious commerce.. also wraps begin rescue end rather than a random rescue on save error.

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -16,19 +16,21 @@ class Purchase < ActiveRecord::Base
   validates_presence_of  :product_id
 
   def save_with_payment
-  if valid?
-    # email = params[user]['email']
-    # customer = Stripe::Customer.create(description: email ,purchase_id: purchase_id,  card: stripe_card_token)
-    customer = Stripe::Customer.create(description: self.product_id)
-    self.stripe_customer_token = customer.id
-    self.stripe_transaction_id = customer.id
-    # self.stripe_customer_token = customer.id
-    save!
-  end
-  rescue Stripe::InvalidRequestError => e
-    logger.error "Stripe error while creating customer: #{e.message}"
-    errors.add :base, "There was a problem with your credit card."
-    false
+    begin
+      if valid?
+        # email = params[user]['email']
+          # customer = Stripe::Customer.create(description: email ,purchase_id: purchase_id,  card: stripe_card_token)
+        customer = Stripe::Customer.create(description: self.product_id)
+        self.stripe_customer_token = customer.id
+        self.stripe_transaction_id = customer.id
+        # self.stripe_customer_token = customer.id
+        save!
+      end
+    rescue Stripe::InvalidRequestError => e
+      logger.error "Stripe error while creating customer: #{e.message}"
+      errors.add :base, "There was a problem with your credit card."
+      false
+    end
   end
 
 end
