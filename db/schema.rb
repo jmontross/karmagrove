516cf98b3f6c060ab533f08f03617917593a42fa
Joshua Montross <joshua.montross@gmail.com>
2013-07-30 22:50:39 -0700

    updates schema for db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130716042456) do
+ActiveRecord::Schema.define(:version => 20130727011614) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -143,6 +143,7 @@ ActiveRecord::Schema.define(:version => 20130716042456) do
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
+    t.integer  "facebook_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
