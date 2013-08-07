066f5e3fb3ad70addff11a2091947357e9ba684f
Joshua Montross <joshua.montross@gmail.com>
2013-08-07 16:57:38 -0700

    facebook does not fail when login from front page without it being after a purchase... also still works for when logged in or create account after purchase

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,3 +1,8 @@
 class Batch < ActiveRecord::Base
-  attr_accessible :batch_name, :sales
+  attr_accessible :batch_name, :sales, :state
+
+  # workflow
+  # states of closed, and then marked as processed by balanced process.
+  #
 end
+
