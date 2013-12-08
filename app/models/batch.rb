33290df92314c6fed95000fbeae011ab38c3e4be
joshua montross <joshua.montross@gmail.com>
2013-12-08 12:39:03 -0800

    improves readme and updates batch with state machine using workflow gem to make it begin as open and be able to get closed. #14

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,8 +1,39 @@
 class Batch < ActiveRecord::Base
-  attr_accessible :batch_name, :sales, :state
+  attr_accessible :batch_name, :sales, :state, :workflow_state
+  include Workflow
 
-  # workflow
-  # states of closed, and then marked as processed by balanced process.
-  #
+  workflow do
+    state :open do
+      event :close, transition_to: :closed
+      event :cancel, transition_to: :cancelled
+    end
+    state :closed do
+      event :cancel, transition_to: :cancelled
+    end
+    state :canceled do
+      event :resume, transition_to: :open
+    end
+
+    state :shipped
+  end
+
+  def initialize(arg1,arg2)
+    super(arg1,arg2)
+    @batch = self
+    self.state = "open"
+    self.save
+  end
+
+  def close
+     @batch.state = "closed"
+     @batch.save
+  end
+
+  # def close(allowed_to_close = true)
+  #   # Let it close! ...
+  #   self.state = self.current_state
+  #   self.save
+  #   halt unless allowed_to_close
+  # end
 end
 
--- spec/models/batch_spec.rb
+++ spec/models/batch_spec.rb
@@ -4,22 +4,23 @@ describe Batch do
 
   #14
   describe "batch state" do
-      before :each do
+    before :each do
       @batch = Batch.create()
     end
 
     it "should begin with a state of open" do
-      @batch.state.should == "open"
+      @batch.current_state.to_s.should == 'open'
     end
 
     it "should be able to be closed" do
       @batch.close!
-      @batch.state.should == "closed"
+      @batch.current_state.to_s.should == 'closed'
+      @batch.state.should == 'closed'
     end
 
     it "should not allow state other than open or closed" do
       @batch.state = "dead"
-      @batch.state.should == "closed"
+      @batch.current_state.to_s.should == "open"
     end
 
   end
