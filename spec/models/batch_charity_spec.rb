afaf50e88444ff1cc222f646d01e8354d66a4c0f
joshua montross <joshua.montross@gmail.com>
2013-12-08 07:06:14 -0800

    cannot add a new batch charity or have buyers vote after a batch is closed. #15

--- spec/models/batch_charity_spec.rb
+++ spec/models/batch_charity_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe BatchCharity do
-  pending "add some examples to (or delete) #{__FILE__}"
+   describe "batch charity voting" do
+      before :each do
+      @batch = Batch.create
+      @charity = Charity.create(:legal_name => "Cool Charity")
+      @batch_charity = BatchCharity.create(:charity_id => @charity.id, :batch_id => @batch.id)
+    end
+
+    it "should allow votes for an open batch" do
+
+    end
+
+    it "should not allow votes for a closed batch" do
+      @batch.close!
+
+      BatchCharity.create(:charity_id => @charity.id, :batch_id => @batch.id)
+      # should not allow new donation tied to this batch.
+      @purchase = Purchase.create
+      Donation.create(purchase_id => ).should == "Exception"
+
+
+    end
+
+  end
 end
