483ed01209d57479b54974835faf3aafe42c042b
joshua montross <joshuamontross@gmail.com>
2013-12-18 14:38:42 -0800

    updates the page where you save email with a page to check if email is correct, also adds facebook login. todo- make facebook login pass userid to update somehow in a nice way taht is secure #27

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,14 @@ class UsersController < InheritedResources::Base
 
   def email_subscribe
     return "fail" unless params[:email]
-    return EmailSubscriber.subscribe params[:email]
+    @email = params[:email]
+    if User.exists?(:email => params[:email])
+      @user = User.find_by_email params[:email]
+    else
+      @user, @status = EmailSubscriber.subscribe params[:email]
+      return @status
+    end
+
   end
 
 
