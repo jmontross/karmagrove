ba3e7074cfefa798f89908b8bba5f004b139df04
joshua montross <joshuamontross@gmail.com>
2013-12-25 18:24:25 -0800

    buyers cannot vote when batch is closed #15

--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Donation do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @batch = Batch.create(:batch_name => "test karma coin batch")
+    @product = Product.create(:name => "Karma Coin")
+  end
+  it "should allow donation for a batch that is open" do
+    purchase = @batch.new_purchase(@product.id)
+    donation = Donation.new(:purchase_id => purchase.id, :charity_id => 1006721, :amount => 200)
+    donation.should.exist == true
+  end
+
+  it "should not allow donation for a batch that is closed" do
+    @batch.close
+    purchase = @batch.new_purchase(@product.id)
+    donation = Donation.new(:purchase_id => purchase.id, :charity_id => 1006721, :amount => 200)
+    donation.should.exist == false
+  end
+
 end
