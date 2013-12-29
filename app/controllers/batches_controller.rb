eb60de50f5dad255419795a199db7828b10fbf24
joshua montross <joshuamontross@gmail.com>
2013-12-28 21:33:32 -0800

    ads batch charity list in case there is none them charities for a btach.... distract and show something else!

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -6,6 +6,10 @@ class BatchesController < InheritedResources::Base
     puts params[:id]
     charity_ids = BatchCharity.where(:batch_id => params[:id].to_i).map {|batch_charity| batch_charity.charity_id }
     @charities = Charity.find(charity_ids)
+    if @charities.nil?
+      @charities = Charity.find [99837, 99838, 99839]
+    end
+    @batch_charities = BatchCharity.all
     @product_id = Product.where(:name => "Karma Coin").first.id
     @purchase = Purchase.new(:product_id => @product_id)
     @disable_nav = true
@@ -20,7 +24,13 @@ class BatchesController < InheritedResources::Base
     @batch = Batch.find(params[:id])
     puts params[:id]
     charity_ids = BatchCharity.where(:batch_id => params[:id].to_i).map {|batch_charity| batch_charity.charity_id }
+    # puts "charity_ids = #{charity_ids}"
+    # Rails.logger.info "charity_ids #{charity_ids}"
     @charities = Charity.find(charity_ids)
+    if @charities.nil?
+      @charities = Charity.find [99837, 99838, 99839]
+    end
+    @batch_charities = BatchCharity.all
     @product_id = Product.where(:name => "Karma Coin").first.id
     @purchase = Purchase.new(:product_id => @product_id)
     respond_to do |format|
