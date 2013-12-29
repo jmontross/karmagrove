1b272b051ed91535eaff995290e868bdf7430292
joshua montross <joshuamontross@gmail.com>
2013-12-28 22:00:06 -0800

    updates the donation page to not error when it cant find charities...

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,2 +1,23 @@
 class MarketsController < InheritedResources::Base
+    def index
+      @disable_sidebar= true
+    @markets = Market.all
+    if params["name"] && Market.find_by_name(params["name"]).nil? == false
+      puts params["name"]
+      @products = Market.find_all_by_name(params["name"])
+    end
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @products }
+    end
+  end
+  def show
+    @market = Market.find params[:id]
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @purchase }
+    end
+  end
+
 end
