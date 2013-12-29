475c2ca8c857db22bba72856b73f4805fc5efa0f
joshua montross <joshuamontross@gmail.com>
2013-12-29 13:09:13 -0800

    ads back asset pipeline... updates homepage with buy link

--- config/application.rb
+++ config/application.rb
@@ -80,7 +80,7 @@ module Karmagrove
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = false
+    config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
