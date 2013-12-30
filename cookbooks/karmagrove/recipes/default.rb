ac4ec2d74077f9d8e560921908a9c23623104e36
joshua montross <joshuamontross@gmail.com>
2013-12-30 09:29:18 -0800

    updates cookbook with something almost functional

--- cookbooks/karmagrove/recipes/default.rb
+++ cookbooks/karmagrove/recipes/default.rb
@@ -2,7 +2,64 @@
 # Cookbook Name:: karmagrove
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Copyleft 2013, karma grove
 #
-# All rights reserved - Do Not Redistribute
+# All rights are ours - Redistribute at will
 #
+
+
+execute "apt-get-update" do
+  command "sudo apt-get update"
+  ignore_failure true
+  action :nothing
+end
+
+# provides /var/lib/apt/periodic/update-success-stamp on apt-get update
+package "update-notifier-common" do
+  notifies :run, resources(:execute => "apt-get-update"), :immediately
+end
+
+execute "apt-get-update-periodic" do
+  # command "apt-get update"
+  ignore_failure true
+  only_if do
+    File.exists?('/var/lib/apt/periodic/update-success-stamp') &&
+    File.mtime('/var/lib/apt/periodic/update-success-stamp') < Time.now - 86400
+  end
+  notifies :run, resources(:execute => "apt-get-update"), :immediately
+end
+
+# install lots of aptitude packages needed for it all to work.
+%w(git curl imagemagick nginx).each do |pkg|; package pkg; end
+
+# sudo apt-get install imagemagick --fix-missing
+
+
+directory "/opt/karmagrove" do
+  action :create
+end
+
+git "/opt/karmagrove" do
+  user "root"
+  group "root"
+  destination "/opt/karmagrove"
+  # this is not a real repo.... where does reportgen
+  repository "https://jmontross@github.com/jmontross/karmagrove.git"
+  reference "master"
+  action :sync
+  # not_if {File.exists?('#{node['dev']['host_path']}/p3')}
+end
+
+script "install dependencies for kg" do
+  interpreter "bash"
+  user "root"
+  # cwd "#{['p3node']['repo_dir']}"
+  # cwd "/opt/picarro/node.js"
+  cwd "/opt/karmagrove/karmagrove"
+  code <<-EOH
+     bundle install
+     bundle exec rake db:migrate
+     bundle exec rails s
+  EOH
+  ignore_failure true
+end
