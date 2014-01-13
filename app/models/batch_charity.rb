016c94300f7aa21784e6836e62ec608eab65f9d0
joshua montross <joshuamontross@gmail.com>
2014-01-12 22:31:23 -0800

    resque perform method added for processing payments

--- app/models/batch_charity.rb
+++ app/models/batch_charity.rb
@@ -1,3 +1,9 @@
 class BatchCharity < ActiveRecord::Base
   attr_accessible :batch_id, :charity_id
+
+  def perform
+  # logic for finding the batch if it is closed,
+  # and then paying each charity by summing all the amounts owed to that charity from different batches
+  end
+
 end
