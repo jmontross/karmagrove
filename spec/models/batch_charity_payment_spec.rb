b1a943c27e28fc28a092db09d61f78f84bbfc5e5
joshua montross <joshuamontross@gmail.com>
2014-02-04 17:06:13 -0800

    batch payments test improved, #65

--- spec/models/batch_charity_payment_spec.rb
+++ spec/models/batch_charity_payment_spec.rb
@@ -3,30 +3,30 @@ require 'spec_helper'
 describe BatchCharityPayment do
   pending "add some examples to (or delete) #{__FILE__}"
 
-  before do 
+  before do
    @batch_complete = Batch.create(:batch_name => "test batch that is done", :sales => 1000, :state => "closed")
    @batch_complete_two = Batch.create(:batch_name => "another test batch that is done", :sales => 613, :state => "closed")
-   @batch_incomplete =  Batch.create(:batch_name => "test batch that is done", :sales => 1000)   
+   @batch_incomplete =  Batch.create(:batch_name => "test batch that is done", :sales => 1000)
   end
 
   it "should find all complete batches when given a new batch_charity_payment object that calls the process method" do
     @batch_charity_payment = BatchCharityPayment.create()
     # not sure if it has status ... @batch_charity_payment.status.should == "something"
-    @batch_charity_payment.process.should == "success"	
-  end 
-  
+    @batch_charity_payment.process.should == "success"
+  end
+
 
   it "should find all batches with the state 'closed'" do
-    #@batch_charity_payment = BatchCharityPayment.create()   
-    BatchCharityPayment.closed_batches.should ~= [@batch_complete,@batch_complete_two]
+    #@batch_charity_payment = BatchCharityPayment.create()
+    BatchCharityPayment.closed_batches.should =~ [@batch_complete,@batch_complete_two]
     BatchCharityPayment.closed_batches.include?(@batch_incomplete).should == false
   end
 
   it "should figure out the charities for each of the closed batches and determine the distribution in dollars to each charity for this batch" do
-   
+
   end
-  
-  it "should determine the distribution in dollars to each charity for this batch " do 
+
+  it "should determine the distribution in dollars to each charity for this batch " do
 
   end
 
