6cc6f2489155cae39ede7ddde323547741e7b4ab
joshua montross <joshuamontross@gmail.com>
2014-02-08 11:49:50 -0800

    product shoudld have an image_url

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :name, :price, :id
+  attr_accessible :description, :name, :price, :id, :image_url
 
   def pretty_price
     printf('%.2f', price)
--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Purchase do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @product = Product.create!({
+      :name => "Karma Coin",
+      :description => "Joy coins are sweet, salty, granola coins consisting of the following ingredients: Oats, Organic Hemp Seed, Honey, Nature's balance, Coconut Crystal Sugar, Walnuts, Chocolate, Coconut Butter",
+      :price => "2.00",
+      :image_url => "/assets/karma_coin_hand.gif"
+    })
+  end
+
+  it "should allow purchase of a product" do
+    @purchase = Purchase.create(:product_id => @product.id)
+    @purchase.product.image_url.should == "/assets/karma_coin_hand.gif"
+
+  end
+
 end
