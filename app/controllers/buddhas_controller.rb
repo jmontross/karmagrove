dfc66a33c41086a69edfde11f953498f45c6128f
joshua montross <joshuamontross@gmail.com>
2014-02-17 14:29:15 -0800

    teaching of buddha product work #66

--- app/controllers/buddhas_controller.rb
+++ app/controllers/buddhas_controller.rb
@@ -1,2 +1,14 @@
 class BuddhasController < InheritedResources::Base
+
+  def dharmaya
+    @product = Product.create(:name => "Teaching of Buddha")
+    @purchase = Purchase.create(:product_id => @product.id)
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @purchase }
+    end
+
+  end
+
 end
