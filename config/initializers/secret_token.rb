9490098b9f458ea12dbc63c100d5c7eb8fe63312
Joshua Montras <joshua.mantras@gmail.com>
2014-05-10 03:02:46 -0500

    moves secret for the rails app to environment variable and provides one in the example config closes #86

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Karmagrove::Application.config.secret_token = 'fd6c8e0ceefb29af0227623335d3ae3736ee1e5c2e310bef32b15142ea3c48c7411410a159c7a95f09242852dc49bde1da81f2f7a71ccfeaf1de2882c59ffa11'
+Karmagrove::Application.config.secret_token = ENV['RAILS_SECRET']
