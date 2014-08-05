483030bd56cdb15c28b8f493b534b3fad7e4d86c
Joshua Montras <joshua.mantras@gmail.com>
2014-08-04 17:31:02 -0700

    new specs for the auction

--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -1,5 +1,49 @@
 require 'spec_helper'
 
 describe Auction do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe " auction creation " do
+      before :each do
+      @auction = Auction.create
+      @charity = Charity.create(:legal_name => "Cool Charity")
+      
+    end
+
+    it "should allow a buy_now price for the seller to change" do
+
+
+    end
+
+    it "should close the auction if a buyer chooses buy_now" do 
+
+    end
+
+    it "should allow the seller to choose a one to a 3 charities for the buyer to choose from" do
+
+    end
+
+    it "should not allow a user to bid on their own auction" do
+    
+    end
+
+    it "should allow a user to bid on the auction" do
+
+    end
+
+    it "should know the winning bid user" do
+
+    end
+
+    it "should notify the seller and buyer at the end of the auction" do
+
+
+    end
+
+    it "should let the buyer choose a charity after they win the auction" do 
+
+    end
+
+    it "should "
+
+
+
 end
