c5f438d10ecb4b57a30347fc4de88b6be55edffc
Joshua Montross <joshua@joshuas-air.attlocal.net>
2014-10-23 17:55:15 -0500

    updates app model for all donations and homepage view to have information about our doations and encourage feedback

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -5,5 +5,11 @@ class Donation < ActiveRecord::Base
 
   attr_accessible :amount, :charity_id, :donation_id, :donation_date, :purchase_id
   #, :charity_payment_id
+  
+  def self.total_donations
+  	total = 0
+    Donation.all.each {|this| total += this.amount.to_i }
+    return total
+  end
 
 end
