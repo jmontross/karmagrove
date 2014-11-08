9448b50d727b579060ead3d9ef7d4971d52cc966
Joshua Montross <joshua.montross@gmail.com>
2014-11-08 15:44:44 -0800

    updates db/seeds to have admin user for active admin in testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
+AdminUser.create! do |a|
+a.email = 'admin@karmagrove.com'
+a.password = a.password_confirmation = 'freedom'
+end
+# Create default user
+User.create! do |u|
+u.username = 'user'
+u.email = 'user@karmagrove.com'
+u.password = u.password_confirmation = 'password'
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
